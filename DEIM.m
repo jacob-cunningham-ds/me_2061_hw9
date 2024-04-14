0f27d4a9e3c07793bb0e4c03698412222755b33d
Jacob Cunningham <Jacobian.Cunningham@proton.me>
2024-04-14 10:22:51 -0400

    homework prompt and support files added

---
+++ DEIM.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:21a0fcfe48bd0cf79c5d2966fbf44003edb007eb44fb37e66c8180749deca4af
+size 147
---
+++ drive.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f08187f674977bffc45bfd8a74cdae8099dd2566d80f1d7b3eb92925bd300cc6
+size 402
